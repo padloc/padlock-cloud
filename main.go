9badb111885a892e66ab5dc2b6b66d82da65812a
Martin Kleinschrodt <martin@maklesoft.com>
2015-11-23 15:53:33 +0700

    Some more documentation

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ const defaultDbPath = "./db"
 const defaultAssetsPath = "./assets"
 const defaultPort = 3000
 
+// Loads settings from environment variables
 func loadEnv(storage *LevelDBStorage, emailSender *EmailSender, assetsPath *string, notifyEmail *string) {
 	emailSender.User = os.Getenv("PADLOCK_EMAIL_USERNAME")
 	emailSender.Server = os.Getenv("PADLOCK_EMAIL_SERVER")
@@ -29,6 +30,7 @@ func loadEnv(storage *LevelDBStorage, emailSender *EmailSender, assetsPath *stri
 	}
 }
 
+// Loads templates from given directory
 func loadTemplates(path string) *Templates {
 	return &Templates{
 		template.Must(template.ParseFiles(filepath.Join(path, "activate.txt"))),
@@ -39,18 +41,22 @@ func loadTemplates(path string) *Templates {
 }
 
 func main() {
+	// Initialize dependencies and load config
 	storage := &LevelDBStorage{}
 	sender := &EmailSender{}
 	var assetsPath, notifyEmail string
 	loadEnv(storage, sender, &assetsPath, &notifyEmail)
 	templates := loadTemplates(filepath.Join(assetsPath, "templates"))
 
+	// Parse options from command line flags
 	port := flag.Int("p", defaultPort, "Port to listen on")
 	requireTLS := flag.Bool("https-only", false, "Set to true to only allow requests via https")
 	flag.Parse()
 
+	// Initialize app instance
 	app := NewApp(storage, sender, templates, Config{RequireTLS: *requireTLS, NotifyEmail: notifyEmail})
 
+	// Handle INTERRUPT and KILL signals
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
 	go func() {
@@ -60,6 +66,7 @@ func main() {
 		os.Exit(0)
 	}()
 
+	// Start server
 	log.Printf("Starting server on port %v", *port)
 	app.Start(fmt.Sprintf(":%d", *port))
 }
