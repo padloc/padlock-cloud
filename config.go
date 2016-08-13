1b96040f236b51af7722c7ff085d07b9ba92cdf2
Martin Kleinschrodt <martin@maklesoft.com>
2016-08-13 15:41:24 +0200

    Pass in configuration file path via command line flag

--- config.go
+++ config.go
@@ -1,16 +1,23 @@
 package padlockcloud
 
-import "os"
 import "log"
+import "flag"
 import "io/ioutil"
 import "encoding/json"
 import "github.com/gravitational/configure"
 
 func LoadConfig(configs ...interface{}) {
-	// load config file
-	yamlData, err := ioutil.ReadFile("config.yaml")
-	if err != nil && !os.IsNotExist(err) {
-		log.Fatal(err)
+	var yamlData []byte
+	var err error
+	configPath := flag.String("config", "", "Path to configuration file")
+	flag.Parse()
+
+	if *configPath != "" {
+		// load config file
+		yamlData, err = ioutil.ReadFile(*configPath)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	for _, config := range configs {
@@ -26,6 +33,7 @@ func LoadConfig(configs ...interface{}) {
 			log.Fatal(err)
 		}
 
+		// TODO: Activate as soon as https://github.com/gravitational/configure/issues/18 is fixed
 		// // parse command line arguments
 		// err = configure.ParseCommandLine(config, os.Args[1:])
 		// if err != nil {
